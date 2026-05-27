09699626570dba2371ccf32e39826533e572e3e7
chaewon <devchae10@gmail.com>
2026-05-27 19:11:20 +0900

    npm install -D webpack, webpack용 svgr 지원 설정

--- next.config.ts
+++ next.config.ts
@@ -1,18 +1,45 @@
 import type { NextConfig } from 'next';
+import type { RuleSetRule } from 'webpack';
 
 const nextConfig: NextConfig = {
-  /* config options here */
   reactCompiler: true,
+
+  webpack(config) {
+    const fileLoaderRule = config.module.rules.find(
+      (rule: RuleSetRule | '...') =>
+        rule !== '...' && rule.test instanceof RegExp && rule.test.test('.svg')
+    ) as RuleSetRule | undefined;
+
+    if (!fileLoaderRule) {
+      return config;
+    }
+
+    config.module.rules.push(
+      {
+        ...fileLoaderRule,
+        test: /\.svg$/i,
+        resourceQuery: /url/,
+      },
+      {
+        test: /\.svg$/i,
+        issuer: fileLoaderRule.issuer,
+        resourceQuery: {
+          not: [...((fileLoaderRule.resourceQuery as { not?: RegExp[] })?.not ?? []), /url/],
+        },
+        use: ['@svgr/webpack'],
+      }
+    );
+
+    fileLoaderRule.exclude = /\.svg$/i;
+
+    return config;
+  },
 
   turbopack: {
     rules: {
       '*.svg': {
         loaders: ['@svgr/webpack'],
         as: '*.js',
-      },
-
-      '*.svg?url': {
-        as: '*.svg',
       },
     },
   },
