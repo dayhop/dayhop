0014ceecf5838c521d37017ca9ec0887b4c665dc
chaewon <devchae10@gmail.com>
2026-05-28 19:52:58 +0900

    style: 반응형 스타일 적용

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,25 @@
+// export default function Home() {
+//   return (
+//     <main>
+//       <h1>dayhop</h1>
+//     </main>
+//   );
+// }
+
+import Textarea from '@/components/ui/Textarea/Textarea';
+
 export default function Home() {
   return (
     <main>
-      <h1>dayhop</h1>
+      <Textarea
+        id="review"
+        label="소중한 경험을 들려주세요"
+        placeholder="체험에서 느낀 경험을 자유롭게 남겨주세요"
+        variant="review"
+        maxLength={100}
+        showCount
+      />
+      <Textarea id="description" label="설명" placeholder="체험에 대한 설명을 입력해 주세요" />
     </main>
   );
 }
--- src/components/ui/Textarea/Textarea.tsx
+++ src/components/ui/Textarea/Textarea.tsx
@@ -10,12 +10,12 @@ interface TextareaProps extends TextareaHTMLAttributes<HTMLTextAreaElement> {
 }
 
 const textareaLabelClassName = {
-  default: 'mb-[10px] block text-[16px] font-bold text-[#1F1F22]',
-  review: 'mb-3 block text-[16px] font-bold text-black',
+  default: 'mb-[10px] block text-[16px] font-bold text-[#1F1F22] md:text-base',
+  review: 'mb-3 block text-[16px] font-bold text-black md:text-[18px]',
 };
 
 const textareaVariantClassName = {
-  default: 'h-[140px] py-4 px-5 rounded-2xl shadow-[0_2px_6px_0_rgba(0,0,0,0.02)]',
+  default: 'h-[140px] md:h-[200px] py-4 px-5 rounded-2xl shadow-[0_2px_6px_0_rgba(0,0,0,0.02)]',
   review: 'h-[179px] p-5 rounded-xl shadow-[0_4px_24px_0_rgba(156,180,202,0.20)]',
 };
 
@@ -53,6 +53,7 @@ const Textarea = ({
           {...props}
           className={twMerge(
             'custom-textarea-scrollbar h-full w-full resize-none bg-transparent text-sm leading-[1.8] font-medium outline-none',
+            'md:text-base',
             'placeholder:text-[#9FA0A7]'
           )}
         />
