ca4afaa883785604646632e278b31131ef83d1b1
chaewon <devchae10@gmail.com>
2026-05-28 11:42:21 +0900

    refactor: 토스트 컴포넌트 위치 변경으로 인한 import 폴더위치 변경

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from 'next';
-import Toast from '@/components/common/Toast';
+import Toast from '@/components/ui/Toast';
 import './globals.css';
 
 export const metadata: Metadata = {
