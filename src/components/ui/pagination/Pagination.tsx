420b698c49130cd6715b346a37a9d0acf41da15a
chaemin58 <sulv0309@naver.com>
2026-05-29 23:05:48 +0900

    fix: 코드리뷰 반영

--- src/components/ui/pagination/Pagination.tsx
+++ src/components/ui/pagination/Pagination.tsx
@@ -8,6 +8,9 @@ interface PaginationProps {
   clickNext: () => void;
   clickPage: (page: number) => void;
 }
+
+const SHOWITEM = 5;
+
 export function Pagination({
   paginationCount,
   currentPage,
@@ -15,10 +18,12 @@ export function Pagination({
   clickNext,
   clickPage,
 }: PaginationProps) {
-  const SHOWITEM = 5;
+  let startPage = currentPage - Math.floor(SHOWITEM / 2);
+  startPage = Math.max(1, startPage);
+  startPage = Math.min(startPage, Math.max(1, paginationCount - SHOWITEM + 1));
 
   return (
-    <nav aria-label="pagenation" className="flex h-10 w-80 items-center justify-center gap-1">
+    <nav aria-label="pagination" className="flex h-10 w-80 items-center justify-center gap-1">
       <button
         onClick={clickPrev}
         className="flex h-6 w-6 cursor-pointer items-center justify-center pt-1 disabled:cursor-default disabled:opacity-30"
@@ -28,26 +33,25 @@ export function Pagination({
       </button>
       {paginationCount <= SHOWITEM
         ? Array.from({ length: paginationCount }).map((_, index) => {
-            const isActive = currentPage === index + 1;
+            const pageNumber = index + 1;
+            const isActive = currentPage === pageNumber;
             return (
               <button
-                key={index}
-                onClick={() => clickPage(index + 1)}
+                key={pageNumber}
+                onClick={() => clickPage(pageNumber)}
                 className={`flex h-6 w-6 cursor-pointer items-center justify-center p-2 text-sm ${isActive ? 'font-bold text-black' : 'text-gray-400'}`}
               >
-                {index + 1}
+                {pageNumber}
               </button>
             );
           })
         : Array.from({ length: SHOWITEM }).map((_, index) => {
-            const pageNumber =
-              currentPage >= SHOWITEM + 1 ? currentPage - (SHOWITEM - 1) + index : index + 1;
-
+            const pageNumber = startPage + index;
             const isActive = currentPage === pageNumber;
 
             return (
               <button
-                key={index}
+                key={pageNumber}
                 onClick={() => clickPage(pageNumber)}
                 className={`flex h-6 w-6 cursor-pointer items-center justify-center p-2 text-sm ${isActive ? 'font-bold text-black' : 'text-text-tertiary'}`}
               >
