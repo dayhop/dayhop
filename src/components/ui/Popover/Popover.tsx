aa15ea347183a46549752fb1b97275634409724c
chaewon <devchae10@gmail.com>
2026-06-01 12:53:47 +0900

    feat: Popover 컴포넌트 기본 레이아웃 작업

--- src/components/ui/Popover/Popover.tsx
+++ src/components/ui/Popover/Popover.tsx
@@ -1,1 +1,41 @@
-export const Popover = {};
+'use client';
+
+import { useState } from 'react';
+
+type PopoverItem = {
+  label: string;
+  onClick: () => void;
+  icon?: React.ReactNode;
+  variant?: 'default' | 'delete';
+};
+export interface PopoverProps {
+  trigger: React.ReactNode;
+  items: PopoverItem[];
+}
+
+export const Popover = ({ trigger, items }: PopoverProps) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <div>
+      <button onClick={() => setIsOpen((prev) => !prev)}>{trigger}</button>
+
+      {isOpen && (
+        <div>
+          {items.map((item) => (
+            <button
+              key={item.label}
+              onClick={() => {
+                item.onClick();
+                setIsOpen(false);
+              }}
+            >
+              {item.icon}
+              {item.label}
+            </button>
+          ))}
+        </div>
+      )}
+    </div>
+  );
+};
