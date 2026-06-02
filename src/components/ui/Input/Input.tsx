2ab73db46620ef04ed69e22e5cc5bd0abb6fb2a1
pho9902 <bagh9902@gmail.com>
2026-06-02 11:56:18 +0900

    feat: 공통 Input props 타입 설계

--- src/components/ui/Input/Input.tsx
+++ src/components/ui/Input/Input.tsx
@@ -1,3 +1,18 @@
-export default function Input() {
-  return <input />;
+interface InputProps extends Omit<React.InputHTMLAttributes<HTMLInputElement>, 'prefix'> {
+  warningText?: string;
+  prefix?: React.ReactNode;
+  suffix?: React.ReactNode;
+  isWarning?: boolean;
+  isDisabled?: boolean;
+}
+
+export default function Input({
+  warningText,
+  prefix,
+  suffix,
+  isWarning,
+  isDisabled,
+  ...props
+}: InputProps) {
+  return <input {...props} />;
 }
