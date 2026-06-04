c22d068db9e040811b8d284f8c8cec351c57d9da
chaemin58 <sulv0309@naver.com>
2026-06-05 01:51:11 +0900

    feat: 로그인 함수 호출 후 액세스 토큰과 리프레시 토큰 반환까지 구현

---
+++ src/components/blocks/LoginForm.tsx
@@ -0,0 +1,124 @@
+'use client';
+
+import { useState } from 'react';
+import Input from '../ui/Input';
+import { Button } from '@/components/ui/Button';
+import instance from '@/lib/api/instance';
+
+export function LoginForm() {
+  const [errorMessage, setErrorMessage] = useState({
+    email: '',
+    password: '',
+  });
+
+  const [isError, setIsError] = useState({
+    email: false,
+    password: false,
+  });
+
+  const [formData, setFormData] = useState({
+    email: '',
+    password: '',
+  });
+
+  const validateField = (id: string, value: string) => {
+    let errorMessage = '';
+    if (id === 'email') {
+      const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+      if (!emailRegex.test(value)) {
+        errorMessage = '올바른 이메일 형식이 아닙니다.';
+        setIsError((prev) => ({ ...prev, email: true }));
+      } else {
+        setIsError((prev) => ({ ...prev, email: false }));
+      }
+    }
+    if (id === 'password') {
+      if (value.length < 8) {
+        errorMessage = '비밀번호는 8자 이상이어야 합니다.';
+        setIsError((prev) => ({ ...prev, password: true }));
+      } else {
+        setIsError((prev) => ({ ...prev, password: false }));
+      }
+    }
+    setErrorMessage((prev) => ({
+      ...prev,
+      [id]: errorMessage,
+    }));
+    console.log(errorMessage);
+  };
+
+  const handleFocusout = (e: React.FocusEvent<HTMLInputElement>) => {
+    validateField(e.target.id, e.target.value);
+  };
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setFormData((prev) => ({ ...prev, [e.target.id]: e.target.value }));
+    console.log(formData);
+  };
+
+  const handleClickButton = async (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+    const res = await postLogin(formData);
+    console.log(res);
+  };
+
+  // ============= 삭제해야할 부분 =====================
+  interface LoginRequest {
+    email: string;
+    password: string;
+  }
+  interface User {
+    id: number;
+    email: string;
+    nickname: string;
+    profileImageUrl: string | null;
+    createdAt: string;
+    updatedAt: string;
+  }
+  interface LoginResponse {
+    user: User;
+    refreshToken: string;
+    accessToken: string;
+  }
+  const postLogin = async (body: LoginRequest): Promise<LoginResponse> => {
+    const { data } = await instance.post<LoginResponse>('/auth/login', body);
+
+    return data;
+  };
+  // =================================================
+
+  return (
+    <div className="flex w-full max-w-140 flex-col gap-5">
+      <div className="flex flex-col gap-2">
+        <label htmlFor="email" className="text-sm font-semibold text-gray-700">
+          이메일
+        </label>
+        <Input
+          id="email"
+          warningText={errorMessage.email}
+          isWarning={isError.email}
+          placeholder="이메일을 입력해 주세요."
+          onBlur={handleFocusout}
+          onChange={handleChange}
+        />
+      </div>
+      <div className="flex flex-col gap-2">
+        <label htmlFor="password" className="text-sm font-semibold text-gray-700">
+          비밀번호
+        </label>
+        <Input
+          type="password"
+          id="password"
+          warningText={errorMessage.password}
+          onBlur={handleFocusout}
+          isWarning={isError.password}
+          placeholder="비밀번호를 입력해주세요."
+          onChange={handleChange}
+        />
+      </div>
+      <Button disabled={isError.email || isError.password} onClick={handleClickButton}>
+        로그인하기
+      </Button>
+    </div>
+  );
+}
