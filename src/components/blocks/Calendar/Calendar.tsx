022fa9deedd8c9865b7135123475e366a00517a0
chaewon <devchae10@gmail.com>
2026-06-06 18:57:25 +0900

    feat: 캘린더 날짜 생성 유틸 함수 구현

--- src/components/blocks/Calendar/Calendar.tsx
+++ src/components/blocks/Calendar/Calendar.tsx
@@ -16,4 +16,24 @@ type CalendarProps = {
   renderDateCell?: (dateInfo: CalendarDateInfo) => React.ReactNode;
 };
 
+const getCalendarDates = (year: number, month: number) => {
+  const firstDate = new Date(year, month, 1);
+  const lastDate = new Date(year, month + 1, 0);
+
+  const firstDay = firstDate.getDay();
+  const lastDay = lastDate.getDay();
+
+  const startDate = new Date(year, month, 1 - firstDay);
+  const endDate = new Date(year, month, lastDate.getDate() + (6 - lastDay));
+
+  const diffTime = endDate.getTime() - startDate.getTime();
+  const diffDate = diffTime / (1000 * 60 * 60 * 24);
+
+  return Array.from({ length: diffDate + 1 }, (_, index) => {
+    const date = new Date(startDate);
+    date.setDate(startDate.getDate() + index);
+    return date;
+  });
+};
+
 export const Calendar = () => {};
