b9cb38108d2cdba2f69968dc7bbe9cb95198aee9
chaewon <devchae10@gmail.com>
2026-06-08 22:31:50 +0900

    refactor: 공휴일 조회 API를 fetch에서 axios로 변경

--- src/hooks/useKoreanHolidays.ts
+++ src/hooks/useKoreanHolidays.ts
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import { useEffect, useState } from 'react';
 
 type Holiday = {
@@ -12,8 +13,7 @@ export const useKoreanHolidays = (year: number, month: number) => {
     const fetchHolidays = async () => {
       const monthValue = String(month + 1).padStart(2, '0');
 
-      const response = await fetch(`/api/holidays?year=${year}&month=${monthValue}`);
-      const data: Holiday[] = await response.json();
+      const { data } = await axios.get<Holiday[]>(`/api/holidays?year=${year}&month=${monthValue}`);
 
       setHolidays(data.map((holiday) => holiday.date));
     };
