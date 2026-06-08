27e2121ff74a3d7da2ad16c00ee16a50200bb654
chaemin58 <sulv0309@naver.com>
2026-06-08 16:41:22 +0900

    fix: 코드리뷰 반영

--- src/components/ui/ReservationList/NavigationButton.stories.tsx
+++ src/components/ui/ReservationList/NavigationButton.stories.tsx
@@ -1,11 +1,38 @@
 import { Meta, StoryObj } from '@storybook/nextjs-vite';
-import { NavigationButton } from './NavigationButton';
+
+import { NavigationButton, ReservationStatus } from './NavigationButton';
+import { useState } from 'react';
 
 const meta: Meta<typeof NavigationButton> = {
   title: 'components/UI/NavigationButton',
   component: NavigationButton,
   tags: ['autodocs'],
+  argTypes: {},
 };
 export default meta;
+
 type Story = StoryObj<typeof NavigationButton>;
-export const Default: Story = {};
+
+export const Default: Story = {
+  args: {
+    activeStatus: '전체 조회',
+  },
+
+  render: (args) => {
+    const [activeStatus, setActiveStatus] = useState<ReservationStatus>(args.activeStatus);
+
+    return (
+      <NavigationButton
+        {...args}
+        activeStatus={activeStatus}
+        onClickButton={(button) => {
+          setActiveStatus(button);
+
+          if (args.onClickButton) {
+            args.onClickButton(button);
+          }
+        }}
+      />
+    );
+  },
+};
