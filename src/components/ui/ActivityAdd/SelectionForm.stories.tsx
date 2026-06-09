56fa4148b69520c045485d3456814c4ee5fe6c84
chaemin58 <sulv0309@naver.com>
2026-06-09 12:11:02 +0900

    fix: 파일명 수정 및 라벨 추가

--- src/components/ui/ActivityAdd/SelectionForm.stories.tsx
+++ src/components/ui/ActivityAdd/SelectionForm.stories.tsx
@@ -12,6 +12,7 @@ type Story = StoryObj<typeof meta>;
 
 export const Default: Story = {
   args: {
+    label: '카테고리',
     onSelectCategory: (category) => {
       console.log('선택된 카테고리:', category);
     },
@@ -26,4 +27,35 @@ export const Default: Story = {
       </div>
     ),
   ],
+};
+
+export const StartTime: Story = {
+  args: {
+    label: '시작 시간',
+    onSelectCategory: (time) => {
+      console.log('선택된 시간:', time);
+    },
+    list: [
+      '6:00',
+      '7:00',
+      '8:00',
+      '9:00',
+      '10:00',
+      '11:00',
+      '12:00',
+      '13:00',
+      '14:00',
+      '15:00',
+      '16:00',
+      '17:00',
+      '18:00',
+      '19:00',
+      '20:00',
+      '21:00',
+      '22:00',
+      '23:00',
+      '00:00',
+    ],
+    defaultMessage: '0:00',
+  },
 };
