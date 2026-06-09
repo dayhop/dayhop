6472fed3ad1e982f36bbc80c2904638bb2d43113
chaemin58 <sulv0309@naver.com>
2026-06-09 15:42:49 +0900

    feat: 커스텀 datepicker 수정

--- src/components/ui/ActivityAdd/DateField.tsx
+++ src/components/ui/ActivityAdd/DateField.tsx
@@ -1,20 +1,36 @@
 'use client';
 
-import { useState } from 'react';
+import { Dispatch, SetStateAction, useState } from 'react';
 
-export function DateField() {
-  const [inputType, setInputType] = useState('text');
+interface FormDataType {
+  date: string;
+  time: string;
+}
+
+interface DateFiledProp {
+  data?: string;
+  setFormData: Dispatch<SetStateAction<FormDataType>>;
+}
+export function DateField({ data, setFormData }: DateFiledProp) {
+  const [inputType, setInputType] = useState<'text' | 'date'>('text');
   return (
     <div className="flex flex-col gap-2.5">
-      <label className="font-bold" id="aactivityDate">
+      <label className="font-bold" htmlFor="activityDate">
         날짜
       </label>
       <input
         id="activityDate"
-        type={inputType}
-        placeholder="yy/mm/dd"
+        value={data}
+        type={data ? 'date' : inputType}
+        placeholder="yyyy/mm/dd"
         onFocus={() => setInputType('date')}
-        className="box-border w-full min-w-81 rounded-lg border border-gray-300 bg-white px-4 py-3 text-base text-gray-800 transition-all outline-none focus:bg-white [&::-webkit-calendar-picker-indicator]:cursor-pointer [&::-webkit-calendar-picker-indicator]:opacity-50 hover:[&::-webkit-calendar-picker-indicator]:opacity-100"
+        onChange={(e) => setFormData((prev) => ({ ...prev, date: e.target.value }))}
+        onBlur={(e) => {
+          if (!e.target.value) {
+            setInputType('text');
+          }
+        }}
+        className="border-bg-footer text-text-primary w-full min-w-81 rounded-2xl border bg-white px-5 py-4 transition-all outline-none focus:bg-white [&::-webkit-calendar-picker-indicator]:cursor-pointer hover:[&::-webkit-calendar-picker-indicator]:opacity-80"
       />
     </div>
   );
