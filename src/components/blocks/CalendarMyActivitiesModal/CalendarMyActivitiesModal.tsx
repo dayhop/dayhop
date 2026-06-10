50b7efe5ff18ab2d3ba6e968e3de6afe15e1c0a9
chaewon <devchae10@gmail.com>
2026-06-10 17:03:29 +0900

    feat: className props 가져오기

--- src/components/blocks/CalendarMyActivitiesModal/CalendarMyActivitiesModal.tsx
+++ src/components/blocks/CalendarMyActivitiesModal/CalendarMyActivitiesModal.tsx
@@ -4,11 +4,22 @@ import { Modal } from '@/components/ui/Modal';
 
 interface CalendarMyActivitiesModalProps {
   onClose: () => void;
+  className?: string;
+  overlayClassName?: string;
 }
 
-export const CalendarMyActivitiesModal = ({ onClose }: CalendarMyActivitiesModalProps) => {
+export const CalendarMyActivitiesModal = ({
+  onClose,
+  className,
+  overlayClassName,
+}: CalendarMyActivitiesModalProps) => {
   return (
-    <Modal onClose={onClose} ariaLabel="예약 현황">
+    <Modal
+      onClose={onClose}
+      ariaLabel="예약 현황 모달"
+      className={className}
+      overlayClassName={overlayClassName}
+    >
       모달
     </Modal>
   );
