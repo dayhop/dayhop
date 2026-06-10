3ff209a1c23d4e7c7f82cff9a801a4428ad0e066
chaewon <devchae10@gmail.com>
2026-06-10 16:34:42 +0900

    feat: CalendarMyActivitiesModal.tsx 에 Modal 컴포넌트 불러오기

--- src/components/blocks/CalendarMyActivitiesModal/CalendarMyActivitiesModal.tsx
+++ src/components/blocks/CalendarMyActivitiesModal/CalendarMyActivitiesModal.tsx
@@ -1,1 +1,15 @@
-export const CalendarMyActivitiesModal = () => {};
+'use client';
+
+import { Modal } from '@/components/ui/Modal';
+
+interface CalendarMyActivitiesModalProps {
+  onClose: () => void;
+}
+
+export const CalendarMyActivitiesModal = ({ onClose }: CalendarMyActivitiesModalProps) => {
+  return (
+    <Modal onClose={onClose} ariaLabel="예약 현황">
+      모달
+    </Modal>
+  );
+};
