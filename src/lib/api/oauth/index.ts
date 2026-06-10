75ffc72a3ae80125f9976c3b97d398031e91dcd2
pho9902 <pho9902@naver.com>
2026-06-10 13:40:16 +0900

    feat: oauth api 추가

---
+++ src/lib/api/oauth/index.ts
@@ -0,0 +1,31 @@
+import instance from '../instance';
+import * as T from './type';
+
+export const postOauthApp = async (
+  body: T.CreateOauthAppRequest
+): Promise<T.CreateOauthAppResponse> => {
+  const { data } = await instance.post<T.CreateOauthAppResponse>('/oauth/apps', body);
+  return data;
+};
+
+export const postOauthSignUp = async (
+  provider: T.OauthProvider,
+  body: T.SignUpWithOauthRequest
+): Promise<T.SignUpWithOauthResponse> => {
+  const { data } = await instance.post<T.SignUpWithOauthResponse>(
+    `/oauth/sign-up/${provider}`,
+    body
+  );
+  return data;
+};
+
+export const postOauthSignIn = async (
+  provider: T.OauthProvider,
+  body: T.SignInWithOauthRequest
+): Promise<T.SignInWithOauthResponse> => {
+  const { data } = await instance.post<T.SignInWithOauthResponse>(
+    `/oauth/sign-in/${provider}`,
+    body
+  );
+  return data;
+};
