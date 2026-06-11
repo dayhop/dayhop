21f9ca0c39dec5808e1b0b468810662313cf7c20
chaemin58 <sulv0309@naver.com>
2026-06-11 15:56:10 +0900

    fix: 메인페이지 원래대로

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,7 @@
-'use client';
-
-import { DateForm } from '@/components/blocks/ActivityAdd/DateForm';
-import { useState } from 'react';
-
 export default function Home() {
-  const [formdata, setFormdata] = useState({
-    title: '함께 배우면 즐거운 스트릿댄스',
-    schedules: [],
-  });
   return (
     <main>
-      <DateForm formdata={formdata} setFormData={setFormdata} />
+      <h1>dayhop</h1>
     </main>
   );
 }
