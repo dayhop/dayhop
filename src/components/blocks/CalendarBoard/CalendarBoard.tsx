980a9411a9b3ba19237019d6b80fbe15cc1a794f
chaewon <devchae10@gmail.com>
2026-06-13 10:24:57 +0900

    feat: Calendar style 추가

--- src/components/blocks/CalendarBoard/CalendarBoard.tsx
+++ src/components/blocks/CalendarBoard/CalendarBoard.tsx
@@ -1,5 +1,13 @@
 import { Calendar } from '../Calendar/Calendar';
 
 export const CalendarBoard = () => {
-  return <Calendar />;
+  return (
+    <Calendar
+      dayHeaderClassName="border-b border-border-default"
+      dateClassName="border-b border-gray-50 [&:nth-last-child(-n+7)]:border-b-0"
+      selectedClassName="w-[18px] h-[18px] rounded-[2px] md:w-[22px] md:h-[22px] rounded-[4px]"
+      todayClassName="w-[18px] h-[18px] rounded-[2px] md:w-[22px] md:h-[22px] rounded-[4px]"
+      holidayClassName="w-[18px] h-[18px] rounded-[2px] md:w-[22px] md:h-[22px] rounded-[4px]"
+    />
+  );
 };
