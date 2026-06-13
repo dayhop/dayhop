3373b8bb5aa674288fcd97b889cab917aaefb79e
chaewon <devchae10@gmail.com>
2026-06-13 13:59:16 +0900

    feat: getMyActivities api 가져오기

--- src/components/blocks/CalendarBoard/CalendarBoard.tsx
+++ src/components/blocks/CalendarBoard/CalendarBoard.tsx
@@ -1,6 +1,26 @@
+import { ReservationCount } from '@/lib/api/my-activities/type';
 import { Calendar } from '../Calendar/Calendar';
+import { useEffect, useState } from 'react';
+import { getMyActivities } from '@/lib/api/my-activities';
 
 export const CalendarBoard = () => {
+  const [activityId, setActivityId] = useState<number | null>(null);
+  const [dateDataMap, setDateDataMap] = useState<Map<string, ReservationCount>>(new Map());
+
+  useEffect(() => {
+    async function fetchActivities() {
+      try {
+        const data = await getMyActivities();
+        if (data.activities.length > 0) {
+          setActivityId(data.activities[0].id);
+        }
+      } catch {
+        // 글로벌 인터셉터에서 처리
+      }
+    }
+    fetchActivities();
+  }, []);
+
   return (
     <Calendar
       dayHeaderClassName="border-b border-border-default"
