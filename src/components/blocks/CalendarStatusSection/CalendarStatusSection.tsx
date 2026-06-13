ce8f6ba8daec4f01b2b3112590b5266c8f1c237d
chaewon <devchae10@gmail.com>
2026-06-13 22:38:05 +0900

    fix: loading state 추가

--- src/components/blocks/CalendarStatusSection/CalendarStatusSection.tsx
+++ src/components/blocks/CalendarStatusSection/CalendarStatusSection.tsx
@@ -10,6 +10,7 @@ import { CalendarBoard } from '../CalendarBoard';
 export const CalendarStatusSection = () => {
   const [activities, setActivities] = useState<ActivityItem[]>([]);
   const [selectedActivity, setSelectedActivity] = useState<ActivityItem | null>(null);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     async function fetchActivities() {
@@ -21,6 +22,8 @@ export const CalendarStatusSection = () => {
         }
       } catch {
         // 글로벌 인터셉터에서 처리
+      } finally {
+        setIsLoading(false);
       }
     }
     fetchActivities();
@@ -28,28 +31,29 @@ export const CalendarStatusSection = () => {
 
   return (
     <>
-      {selectedActivity ? (
-        <>
-          <SelectField
-            list={activities.map((activity) => activity.title)}
-            onSelectOption={(title) => {
-              const activity = activities.find((activity) => activity.title === title);
-              if (activity) setSelectedActivity(activity);
-            }}
-            selectedOption={selectedActivity?.title ?? ''}
-            defaultMessage="체험을 선택해 주세요"
-          />
-          <div className="mt-7.5 md:mt-6 lg:relative lg:mt-7.5">
-            <CalendarBoard
-              key={selectedActivity.id}
-              activityId={selectedActivity.id}
-              wrapperClassName="md:rounded-3xl md:bg-white md:pt-5 md:pb-2.5 md:shadow-[0_4px_24px_0_rgba(156,180,202,0.2)]"
+      {!isLoading &&
+        (selectedActivity ? (
+          <>
+            <SelectField
+              list={activities.map((activity) => activity.title)}
+              onSelectOption={(title) => {
+                const activity = activities.find((activity) => activity.title === title);
+                if (activity) setSelectedActivity(activity);
+              }}
+              selectedOption={selectedActivity?.title ?? ''}
+              defaultMessage="체험을 선택해 주세요"
             />
-          </div>
-        </>
-      ) : (
-        <EmptyState message="아직 등록한 체험이 없어요" />
-      )}
+            <div className="mt-7.5 md:mt-6 lg:relative lg:mt-7.5">
+              <CalendarBoard
+                key={selectedActivity.id}
+                activityId={selectedActivity.id}
+                wrapperClassName="md:rounded-3xl md:bg-white md:pt-5 md:pb-2.5 md:shadow-[0_4px_24px_0_rgba(156,180,202,0.2)]"
+              />
+            </div>
+          </>
+        ) : (
+          <EmptyState message="아직 등록한 체험이 없어요" />
+        ))}
     </>
   );
 };
