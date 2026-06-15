13178adf8ac01d65890562e5914a9b8f964deab8
chaewon <devchae10@gmail.com>
2026-06-15 16:08:11 +0900

    feat: MenuItem 컴포넌트를 Button에서 Link 형태로 변경

--- src/components/ui/MenuItem/MenuItem.tsx
+++ src/components/ui/MenuItem/MenuItem.tsx
@@ -1,16 +1,28 @@
-import { Button } from '../Button';
+import Link from 'next/link';
+import { cn } from '@/utils/cn';
 
 interface MenuItemProps {
   children: React.ReactNode;
   Icon?: React.ComponentType<React.SVGProps<SVGSVGElement>>;
+  href: string;
+  selected?: boolean;
 }
 
-export const MenuItem = ({ children, Icon }: MenuItemProps) => {
+export const MenuItem = ({ children, Icon, href, selected = false }: MenuItemProps) => {
   return (
-    <>
-      <Button variant="text" Icon={Icon} className="px-5">
-        {children}
-      </Button>
-    </>
+    <Link
+      href={href}
+      className={cn(
+        'hover:bg-primary-100 inline-flex h-13.5 w-full items-center gap-2 rounded-2xl bg-white px-5 font-medium text-gray-600',
+        selected && 'bg-primary-100 text-black'
+      )}
+    >
+      {Icon && (
+        <span className="inline-flex h-5 w-5 items-center justify-center">
+          <Icon className={selected ? 'text-primary' : 'text-gray-600'} />
+        </span>
+      )}
+      <span>{children}</span>
+    </Link>
   );
 };
