092e7fb024c680d06404e8b2aa515721f1b71a26
chaewon <devchae10@gmail.com>
2026-06-16 10:40:10 +0900

    feat: ProfileImage UI 작업

--- src/components/blocks/ProfileImage/ProfileImage.tsx
+++ src/components/blocks/ProfileImage/ProfileImage.tsx
@@ -4,9 +4,9 @@ import EditIcon2 from '@/assets/icon/EditIcon2.svg';
 export const ProfileImage = () => {
   return (
     <div>
-      <button type="button">
-        <Avatar />
-        <EditIcon2 />
+      <button type="button" className="relative cursor-pointer">
+        <Avatar size="lg" />
+        <EditIcon2 className="absolute right-0 bottom-0 md:h-6 md:w-6 lg:h-7.5 lg:w-7.5" />
       </button>
     </div>
   );
