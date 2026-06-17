a0ffb7e60bc8109683ffccaf01ff582f70242e4d
chaewon <devchae10@gmail.com>
2026-06-17 14:31:03 +0900

    feat: PasswordConfirmModal 기본 구조 및 import 추가

---
+++ src/components/blocks/PasswordConfirmModal/PasswordConfirmModal.tsx
@@ -0,0 +1,10 @@
+'use client';
+
+import { useState } from 'react';
+import { Modal } from '@/components/ui/Modal';
+import { Button } from '@/components/ui/Button';
+import Input from '@/components/ui/Input';
+
+export const PasswordConfirmModal = () => {
+  return <></>;
+};
