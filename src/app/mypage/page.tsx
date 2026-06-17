653b2e5f3b54e0b191d05af8ea67214f029b0484
pho9902 <pho9902@naver.com>
2026-06-18 04:38:27 +0900

    feat: 마이페이지 레이아웃 구현

--- src/app/mypage/page.tsx
+++ src/app/mypage/page.tsx
@@ -1,5 +1,9 @@
 import { ProfileCard } from '@/components/blocks/ProfileCard';
 
 export default function MyPage() {
-  return <ProfileCard />;
+  return (
+    <div className="md:hidden">
+      <ProfileCard />
+    </div>
+  );
 }
