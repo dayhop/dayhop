775b69778a39d1e727cc21adca87f35da825ac65
pho9902 <pho9902@naver.com>
2026-06-18 04:21:39 +0900

    chore: 마이페이지 경로, proxy matcher 변경

--- src/components/blocks/MenuArea/MenuArea.tsx
+++ src/components/blocks/MenuArea/MenuArea.tsx
@@ -11,7 +11,7 @@ const MENU_ITEMS = [
   //  TODO: 실제 페이지 URL로 교체 필요
   { label: '내 정보', Icon: UserIcon, href: '/mypage/info' },
   { label: '예약내역', Icon: ListIcon, href: '/mypage/reservations' },
-  { label: '내 체험 관리', Icon: CalendarIcon, href: '/mypage/activities' },
+  { label: '내 체험 관리', Icon: CalendarIcon, href: '/mypage/experiences' },
   { label: '예약현황', Icon: SettingIcon, href: '/mypage/calendar' },
 ];
 
