f6b6c751f41d605a786ab977b85b3119d104537d
chaewon <devchae10@gmail.com>
2026-06-18 16:43:04 +0900

    feat: CalendarPageHeader 추가 및 mypage 예약현황 페이지에 추가

--- src/app/mypage/calendar/page.tsx
+++ src/app/mypage/calendar/page.tsx
@@ -1,5 +1,11 @@
+import { CalendarPageHeader } from '@/components/blocks/CalendarStatusSection/CalenaderPageHeader';
 import { CalendarStatusSection } from '@/components/blocks/CalendarStatusSection';
 
 export default function CalendarPage() {
-  return <CalendarStatusSection />;
+  return (
+    <div className="mt-2.5 flex flex-col">
+      <CalendarPageHeader />
+      <CalendarStatusSection />
+    </div>
+  );
 }
