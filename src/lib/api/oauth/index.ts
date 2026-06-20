bd607975c1cd9a62a84e578456f4bf7dae17a9fa
chaemin58 <sulv0309@naver.com>
2026-06-20 12:53:54 +0900

    fix: secure 프로덕트 모드에서만 켬

--- src/lib/api/oauth/index.ts
+++ src/lib/api/oauth/index.ts
@@ -9,14 +9,14 @@ const setAuthCookies = async (accessToken: string, refreshToken: string) => {
   const cookieStore = await cookies();
   cookieStore.set('accessToken', accessToken, {
     httpOnly: true,
-    secure: true,
+    secure: process.env.NODE_ENV === 'production',
     sameSite: 'lax',
     path: '/',
     maxAge: 60 * 15,
   });
   cookieStore.set('refreshToken', refreshToken, {
     httpOnly: true,
-    secure: true,
+    secure: process.env.NODE_ENV === 'production',
     sameSite: 'lax',
     path: '/',
     maxAge: 60 * 60 * 24 * 7,
