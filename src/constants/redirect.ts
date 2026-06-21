a580108408ed328724d8bd9f86c6cf175bfc5d52
chaemin58 <sulv0309@naver.com>
2026-06-21 15:21:49 +0900

    fix: 실행 환경에 따라 카카오 로그인 리다이렉트 주소 다르도록 변경

---
+++ src/constants/redirect.ts
@@ -0,0 +1,4 @@
+const KAKAO_LOGIN_REDIRECT_URI_LOCAL = 'http://localhost:3000/oauth/kakao/login/';
+const KAKAO_SIGNUP_REDIRECT_URI_LOCAL = 'http://localhost:3000/oauth/kakao/signup/';
+const KAKAO_LOGIN_REDIRECT_URI_VERCEL = 'https://dayhop-amber.vercel.app/oauth/kakao/login/';
+const KAKAO_SIGNUP_REDIRECT_URI_VERCEL = 'https://dayhop-amber.vercel.app/oauth/kakao/signup/';
